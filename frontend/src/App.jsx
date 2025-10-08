96574c92ae60c34307b334430144c1d9e6a8231a
lucas <lucas.clementezanella@gmail.com>
2025-10-07 21:36:31 -0300

    comecando a inserir conteudo na tela, dependencias instaladas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,29 @@
-import React from 'react'
-import Home from "./routes/Home"
-import Contato from "./routes/Contato"
-import Login from "./routes/Login"
-import Planos from "./routes/Planos"
-import Sobre from "./routes/Sobre"
-import Treinos from './routes/Treinos'
-import Error from './routes/Error'
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import Contato from "./routes/Contato";
+import Login from "./routes/Login";
+import Planos from "./routes/Planos";
+import Sobre from "./routes/Sobre";
+import Treinos from "./routes/Treinos";
+import Error from "./routes/Error";
+import Cadastro from "./routes/Cadastro";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home/>}/>
-        <Route path="/contato" element={<Contato/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/planos" element={<Planos/>}/>
-        <Route path="/sobre" element={<Sobre/>}/>
-        <Route path="/treinos" element={<Treinos/>}/>
-        <Route path="*" element={<Error/>}/>
-        
+        <Route path="/" element={<Home />} />      {/* página inicial */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/contato" element={<Contato />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/planos" element={<Planos />} />
+        <Route path="/sobre" element={<Sobre />} />
+        <Route path="/treinos" element={<Treinos />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="*" element={<Error />} />    {/* rota para erros */}
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+// src/routes/Home.jsx
 const Home = () => {
   return (
-    <div>
-      
+    <div className="p-8 text-center bg-black w-screen h-screen
+">
+      <h1 className="text-4xl font-bold text-blue-600">Bem-vindo à Nexus gym!</h1>
+      <p className="mt-4 text-lg text-gray-700">CODANDO MUSCULOS 🚀</p>
     </div>
   )
 }
